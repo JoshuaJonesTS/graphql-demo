093a6353a5f75b12dd2260b962f20088dabb1b8f
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-16 13:37:50 -0800

    Pass client attribute to ApolloProvider

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ import {
 } from '@apollo/client';
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://graphql-weather-api.herokuapp.com/"
+  });
+  
   return (
-    <ApolloProvider>
+    <ApolloProvider client={client}>
       <Home />
     </ApolloProvider>
   );
